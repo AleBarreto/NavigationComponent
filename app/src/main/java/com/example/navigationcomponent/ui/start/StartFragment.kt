245ba506362bed305e88e26f93ccdd7d25a61666
alessandro <alessandro@pop-os.localdomain>
2020-12-15 15:48:29 -0400

    Config JetPack gradle and test navigation

--- app/src/main/java/com/example/navigationcomponent/ui/start/StartFragment.kt
+++ app/src/main/java/com/example/navigationcomponent/ui/start/StartFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.navigationcomponent.R
+import kotlinx.android.synthetic.main.fragment_start.*
 
 class StartFragment : Fragment() {
 
@@ -15,4 +17,14 @@ class StartFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_start, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button_next.setOnClickListener {
+            findNavController().navigate(R.id.action_startFragment_to_profileFragment)
+        }
+
+    }
+
 }
