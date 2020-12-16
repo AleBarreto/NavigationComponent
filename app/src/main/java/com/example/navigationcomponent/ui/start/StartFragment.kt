c5569cacd687f5f6ade79379642554fe14584b00
alessandro <alessandro@pop-os.localdomain>
2020-12-16 15:19:51 -0400

    Create extensions to navigate with animations default

--- app/src/main/java/com/example/navigationcomponent/ui/start/StartFragment.kt
+++ app/src/main/java/com/example/navigationcomponent/ui/start/StartFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.navigationcomponent.R
+import com.example.navigationcomponent.extensions.navigateWithAnimations
 import kotlinx.android.synthetic.main.fragment_start.*
 
 class StartFragment : Fragment() {
@@ -22,7 +23,7 @@ class StartFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         button_next.setOnClickListener {
-            findNavController().navigate(R.id.action_startFragment_to_profileFragment)
+            findNavController().navigateWithAnimations(R.id.action_startFragment_to_profileFragment)
         }
 
     }
